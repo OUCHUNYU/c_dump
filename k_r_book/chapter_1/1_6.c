df96b4fb830194ae9a04f3f0470f65c0aa0c5177
Chunyu Ou <chunyu@unify.id>
2017-12-19 22:48:21 -0800

    finish exercise 1_6 and 1_7

---
+++ k_r_book/chapter_1/1_6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("The \"getchar() != EOF\" is %i \n", (getchar() != EOF));
+    
+    return 0;
+}
---
+++ k_r_book/chapter_1/1_7.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("The EOF has value %d\n", EOF);
+    return 0;
+}
