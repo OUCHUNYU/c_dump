8915fddb91e5a5f18c1211e85e56d4943b3e1f10
Chunyu Ou <chunyu@unify.id>
2018-01-10 05:23:29 -0800

    add half of 4-4

---
+++ k_r_book/chapter_4/4.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+#define MAXVAL 100
+#define BUFSIZE 100
+
+int getop(char s[]);
+int getch();
+void ungetch(int c);
+
+/*
+ * Stack functions
+ */
+void push(double d);
+double pop();
+void print_top();
+
+/*
+ * states
+ */
+int sp = 0;
+double val[MAXVAL];
+char buf[BUFSIZE];
+int bufp = 0;
+
+int main() {
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0) {
+                    push(pop() / op2);
+                } else {
+                    printf("error: zero divisor\n");
+                }
+                break;
+            case '%':
+                op2 = pop();
+                if (op2 != 0.0) {
+                    push(fmod(pop(), op2));
+                } else {
+                    printf("error: zero divisor\n");
+                }
+                break;
+            case '\n':
+                break;
+            case '?':
+                print_top();
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+
+    return 0;
+}
+
+void push(double d) {
+    if (sp < MAXVAL) {
+        val[sp++] = d;
+    } else {
+        printf("error: stack full, can't push %g\n", d);
+    }
+}
+
+double pop() {
+    if (sp > 0) {
+        return val[--sp];
+    } else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+void print_top() {
+    if (sp > 0) {
+        printf("Top element of the stack: %lf\n", val[sp - 1]); 
+    } else {
+        printf("error: stack empty\n");
+    }
+}
+
+int getop(char s[]) {
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t') { }
+
+    s[1] = '\0';
+
+    if (!isdigit(c) && c != '.' && c != '-') {
+        return c;
+    }
+
+    i = 0;
+
+    if (c == '-') {
+        int next = getch();
+        if (!isdigit(next) && next != '.') {
+            return c;
+        }
+        c = next;
+    } else {
+        c = getch();
+    }
+
+    s[++i] = c;
+
+    if (isdigit(c)) {
+       while (isdigit(s[++i] = c = getch())) { }
+    }
+
+    if (c == '.') {
+        while (isdigit(s[++i] = c = getch())) { }
+    }
+
+    s[i] = '\0';
+
+    if (c != EOF) {
+        ungetch(c);
+    }
+
+    return NUMBER;
+}
+
+int getch() {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if (bufp >= BUFSIZE) {
+        printf("ungetch: too many characters\n");
+    } else {
+        buf[bufp++] = c;
+    }
+}
+
+
