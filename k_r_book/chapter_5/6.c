2f25c6cbb6d866349e2012bbff1dc385a082e91d
Chunyu Ou <chunyu@unify.id>
2018-01-16 18:15:52 -0800

    add 5-6 getline and reverse

---
+++ k_r_book/chapter_5/6.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define MAX_GETLINE 100000
+
+int _getline(char *line, int max);
+void _reverse(char *s);
+int _atoi(char *a);
+void _itoa(char *a, int i);
+
+int main() {
+    
+    char test_getline[MAX_GETLINE]; 
+    _getline(test_getline, MAX_GETLINE);
+    printf("test _getline()\n%s", test_getline);
+    
+    _reverse(test_getline);
+    printf("test _reverse()\n%s", test_getline);
+
+    return 0;
+}
+
+int _getline(char *line, int max) {
+    int i = 0, c;
+
+    while ((c = getchar()) && c != '\n' && c != EOF && i < max) {
+        *line = c;
+        line++;
+        i++;
+    } 
+
+    if (c == '\n') {
+        *line = '\n';
+        line++;
+        i++;
+    }
+
+    *line = '\0';
+
+    return i;
+}
+
+void _reverse(char *s) {
+    int index_counter = 0;
+
+    while (*s != '\0') {
+        s++;
+        index_counter++;
+    }
+
+    s -= index_counter;
+
+    int c, i, j;
+    for (i = 0, j = index_counter - 1; i < j; i++, j--) {
+        c = *(s + i);
+        *(s + i) = *(s + j);
+        *(s + j) = c;
+    }
+}
+
+
+
