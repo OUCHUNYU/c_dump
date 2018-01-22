b9d5e87dde5f54df0700c873a85abf915a5656b0
Chunyu Ou <chunyu@unify.id>
2018-01-22 00:01:22 -0800

    modify 5-11 argument parser

--- k_r_book/chapter_5/11.c
+++ k_r_book/chapter_5/11.c
@@ -7,43 +7,46 @@
 #define MAX_TABSTOPS 10
 #define MAX_LINE 10000
 
-void parse_tabstops(int argc, char *argv[], int *arguments);
+int parse_tabstops(int argc, char *argv[], int *arguments);
 int fetch_line(char *line, int max_char_count);
-void entab(char *in, char *out, int *tabstops);
-void detab(char *in, char *out, int *tabstops);
+void entab(char *in, char *out, int *tabstops, int ts_count);
+void detab(char *in, char *out, int *tabstops, int ts_count);
 
 int main(int argc, char *argv[]) {
     int user_provided_tabstops[MAX_TABSTOPS] = {0};
+    int argument_count = parse_tabstops(argc, argv, user_provided_tabstops);
+    int *tabstops = malloc(argument_count * sizeof(int));
+    memcpy(tabstops, user_provided_tabstops, argument_count * sizeof(int));
 
-    parse_tabstops(argc, argv, user_provided_tabstops);
+    // for (int i = 0; i < argument_count; i++) {
+    //     printf("argument %i: %i\n", i, tabstops[i]);
+    // }
 
-    for (int i = 0; i < MAX_TABSTOPS; i++) {
-        printf("argument %i: %i\n", i, user_provided_tabstops[i]);
+    char current_line[10000];
+    char output_line[10000];
+    
+    while (fetch_line(current_line, MAX_LINE) > 0) {
+        puts("-------------- start -----------------\n");
+        printf("original: %s\n", current_line);
+        entab(current_line, output_line, tabstops, argument_count);
+        printf("entabed: %s\n", output_line);
+        entab(current_line, output_line, tabstops, argument_count);
+        printf("detabed: %s\n", output_line);
+        puts("--------------- end ------------------\n");
     }
 
-    // char current_line[10000];
-    // char output_line[10000];
-    // 
-    // while (fetch_line(current_line, MAX_LINE) > 0) {
-    //     puts("-------------- start -----------------\n");
-    //     printf("original: %s\n", current_line);
-    //     entab(current_line, output_line, user_provided_tabstops);
-    //     printf("entabed: %s\n", output_line);
-    //     entab(current_line, output_line, user_provided_tabstops);
-    //     printf("detabed: %s\n", output_line);
-    //     puts("--------------- end ------------------\n");
-    // }
+    free(tabstops);
 
     return 0;
 }
 
-void parse_tabstops(int argc, char *argv[], int *arguments) {
+int parse_tabstops(int argc, char *argv[], int *arguments) {
     // skip file name part
     argc--;
     argv++;
 
     if (argc <= 0) {
-        return;
+        return 0;
     }
 
     if (**argv != '-' || *(*argv + 1) != 't') {
@@ -58,6 +61,7 @@ void parse_tabstops(int argc, char *argv[], int *arguments) {
     char found_tabstop_str[100];
     int last_found_index = 0;
     int i;
+    int counter = 0;
 
     for (i = 0; (current_c = *(tabstop_list + i)) != '\0'; i++) {
         if (current_c != ',' && !isdigit(current_c)) {
@@ -68,17 +72,40 @@ void parse_tabstops(int argc, char *argv[], int *arguments) {
         } else {
             strncpy(found_tabstop_str, tabstop_list + last_found_index, i - last_found_index);
             *(found_tabstop_str + i - last_found_index) = '\0'; // add a terminator to copied str
-            printf("copied str: %s\n", found_tabstop_str);
             last_found_index = i + 1;
             *arguments = atoi(found_tabstop_str);            
             arguments++;
+            counter++;
         }
     }
     // add last num before \0
     strncpy(found_tabstop_str, tabstop_list + last_found_index, i - last_found_index);
     *(found_tabstop_str + i - last_found_index) = '\0';
-    printf("copied str: %s\n", found_tabstop_str);
     *arguments = atoi(found_tabstop_str);            
+    counter++;
+    return counter;
 }
+
+int fetch_line(char *line, int max_char_count) {
+    int current_char;
+    int size = 0;
+    while ((current_char = getchar()) != EOF && size <= max_char_count) {
+        line[size] = current_char;
+        size ++;
+        if (current_char == '\n') {
+            break;
+        }
+    }
+
+    line[size] = '\0';
+    return size;
+}
+
+void entab(char *in, char *out, int *tabstops, int ts_count) {
+}
+
+void detab(char *in, char *out, int *tabstops, int ts_count) {
+}
+
 
 
