b2a41c48c9dc94f884ef865b8282d86ff072a3a1
Chunyu Ou <chunyu@unify.id>
2018-03-27 23:20:08 -0700

    add string const handling to chap 6 ex1

--- k_r_book/chapter_6/1.c
+++ k_r_book/chapter_6/1.c
@@ -11,6 +11,8 @@ int getch();
 void ungetch(int ch);
 
 int get_word(char *word_buff, int limit);
+int is_underscore(int ch);
+int handle_string_const(char *word_buff, int current_c, int *limit);
 
 int main(int argc, char *argv[]) {
 
@@ -18,7 +20,7 @@ int main(int argc, char *argv[]) {
 
 	while(1) {
 		int result = get_word(word_buff, MAX_WORD_SIZE);
-		printf("word --> %s, result --> %d\n", word_buff, result);
+		printf("word --> %s\n", word_buff);
 	}
 
 	return 0;
@@ -49,6 +51,7 @@ void ungetch(int ch) {
 int get_word(char *word_buff, int limit) {
 	int current_c;
 	char *temp_word = word_buff;
+	int limit_copy = limit;
 
 	while (isspace(current_c = getch())) {
 		// NOP
@@ -58,14 +61,18 @@ int get_word(char *word_buff, int limit) {
 		*temp_word = current_c;
 		temp_word++;
 	}
+	
+	current_c = handle_string_const(temp_word, current_c, &limit);
 
-	if (!isalpha(current_c)) {
-		*temp_word = '\0';
+	// TODO: handle comments like // or /* */
+
+	if (!isalpha(current_c) && !is_underscore(current_c)) {
+		*(temp_word += (limit_copy - limit)) = '\0';
 		return current_c;
 	}
 
 	for (; --limit > 0; temp_word++) {
-		if (!isalnum(*temp_word = getch())) {
+		if (!isalnum(*temp_word = getch()) && !is_underscore(*temp_word)) {
 			ungetch(*temp_word);
 			break;
 		}
@@ -75,10 +82,34 @@ int get_word(char *word_buff, int limit) {
 	return word_buff[0];
 }
 
+int is_underscore(int ch) {
+	return ch == '_';
+}
 
+int handle_string_const(char *word_buff, int current_c, int *limit) {
+	// early exit if not a ", return the same char back so the checking can continue
+	if (current_c != '\"') {
+		return current_c;
+	}
 
+	// *word_buff = current_c;
+	// word_buff++;
+	for (; --(*limit) > 0; word_buff++) {
+		*word_buff = getch();
+		if (*word_buff == '\"') {
+			break;
+		}
+	}
 
+	if (*word_buff != '\"') {
+		printf("Error: ungetch exceed the max size allowed but still no ending \" \n");
+		exit(EXIT_FAILURE);
+	}
 
+	int result = *word_buff;
+	word_buff++;
+	return result;
+}
 
 
 
