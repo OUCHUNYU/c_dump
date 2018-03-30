931ba504dfeb49d7e6baf22f00669588fe42262a
Chunyu Ou <chunyu@unify.id>
2018-03-30 08:36:29 -0700

    scafold 6-2

---
+++ k_r_book/chapter_6/2.c
@@ -0,0 +1,63 @@
+/*
+
+Exercise 6-2. Write a program that reads a C program and prints in alphabetical 
+order each group of variable names that are identical in the first 6 characters, 
+but different somewhere thereafter. Don’t count words within strings and 
+comments. Make 6 a parameter that can be set from the command line.
+
+Solution:
+make a structure to hold the group
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_GROUP_ALLOWED 1000
+#define MAX_GROUP_LIST_SIZE 1000
+
+struct group {
+	char *identical_chars;
+	int identical_char_count;
+	char *list[MAX_GROUP_LIST_SIZE];
+};
+
+int parse_arg(char *argv[]);
+struct group *init_new_group(char *first_string, int count);
+int check_identical(char *str1, char *str2, int count);
+
+int main(int argc, char *argv[]) {
+
+	int default_identical_char_count = 6;
+
+	if (argc > 1) {
+		default_identical_char_count = parse_arg(argv);
+	}
+
+	struct group *ordered_groups[MAX_GROUP_ALLOWED];
+
+	/* 
+		get a variable in each loop
+		go through the groups and check if it belongs to any group
+		if yes put it in that group
+		else create a new group and put it in the groups
+
+		at the end, go through the groups again to print each group
+	*/
+
+	return 0;
+}
+
+int parse_arg(char *argv[]) {
+	int result = atoi(argv[1]);
+	printf("Argument received, default identical character count is now %d\n", result);
+	return result;
+}
+
+
+
+
+
+
+
